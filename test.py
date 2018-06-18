2f981b25f1077a8a993c3917c9f48a11bf767de0
nikanar <nikana496@gmail.com>
2018-06-18 12:09:18 -0400

    graph MR-by-ID when testing, from the log file

--- test.py
+++ test.py
@@ -7,11 +7,12 @@ import os
 os.environ['CUDA_VISIBLE_DEVICES']='0'
 os.environ['CONDA_PREFIX']=''
 
+
 def main():
     kb = 'RV15M' # FB15K
     model = "TransE" # DistMult
     clusters = "V2"
-
+    
     con = config.Config()
     con.set_in_path("/u/wujieche/Projects/OpenKE/data/"+kb+"/")
     con.set_test_link_prediction(True)
@@ -25,45 +26,31 @@ def main():
     log_path = "{}-{}_clusters_test_log.log".format(model, kb)
     os.rename("log.log", log_path)
 
-    # rank_by_relID_plot()
+    rank_by_relID_plot(log_path)
 
 def read_log(path):
-    log = []
+    import pandas as pd
     with open(path) as f:
-        for l in f:
-            arg1, rel, arg2, head_rank, tail_rank = l.split()
-            log.append([rel, head_rank, tail_rank])
-    return log
+        data = [l.split() for l in f]
+    columns = ['arg1_id', 'rel_id', 'arg2_id', 'head_rank', 'tail_rank']
+    df = pd.DataFrame.from_records(data, columns=columns)
+    return df
 
 def rank_by_relID_plot(log_path):
-    log = read_log(log_path)
     import matplotlib.pyplot as plt
-    head_ranks = [hr for r, hr, _ in log]
-    tail_ranks = [tr for r, _, tr in log]
-    ids = [r for r, _, __ in log]
+    df = read_log(log_path)
+    df = df.drop(columns=["arg1_id", "arg2_id"])
+    # max_r = max(df['rel_id'])
+    # df['ranked_ids'] = df['rel_id'].rank(method='first')
 
-    # FIXME Make a pandas dataframe directly out of the log data
-    # (because pandas has nice data processing functions useful for graphing)
-    
-    # df = pd.read_csv("log_no_cluster.csv")
-    # df = df.drop(columns=["id", "index", "h", "t"])
-    # max_r = max(df['r'])
-    # # print(max_r)
+    bins = 30
+    grp = df.groupby(by = pd.qcut(df['rel_id'], bins))
+    df = grp.aggregate(np.average)
 
-    # df['ranked_ids'] = df['r'].rank(method='first')
-    # # df['x'] = pd.qcut(df['ranked_ids'], 1000)
-
-    
-    freq_head_MR, freq_tail_MR = 169512, 237961
-    darkblue, darkgreen = "#3030AA", "#40AA40"
-
-    # bins = 30
-    # grp = df.groupby(by = pd.qcut(df['r'], bins))
-    # df = grp.aggregate(np.average)
-
-    
     plt.plot(ids, tail_ranks, "b", label = "arg1 mean rank")
     plt.plot(ids, head_ranks, "g", label = "arg2 mean rank")
+    freq_head_MR, freq_tail_MR = 169512, 237961
+    darkblue, darkgreen = "#3030AA", "#40AA40"
     plt.hlines([freq_head_MR, freq_tail_MR], xmin=0, xmax = 10000, colors = [darkblue, darkgreen], label = "baseline")
     plt.legend()
     plt.savefig(log_path[:-4]+".MR_by_ID-graph.png")
